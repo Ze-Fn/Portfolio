4491ca1b0f9c9fddaf2d63711836277884030166
Ze-Fn <zelvy.fauzan2000@gmail.com>
2025-12-25 20:59:20 +0700

    Restructured the dir and subdirs

---
+++ Python General/Basic Operations.py
@@ -0,0 +1,20 @@
+def area_rect():
+    '''
+    This program calculates the area of a rectangular of any given side measurement (cm).
+    '''
+    x = float(input("Insert the width: "))
+    y = float(input("Insert the length: "))
+    return x * y
+
+print(f"The area of your rectangle is {area_rect()} cm^2")
+
+# def area_rect():
+#     '''
+#     Calculates the area of a rectangle.
+#     '''
+#     x = float(input("Insert the width: "))
+#     y = float(input("Insert the length: "))
+#     return x * y
+
+# area = area_rect()
+# print(f"The area is {area} cm^2")
